fcd2a3781d3942055791163a0aa7b8829f95e112
gpestana <g6pestana@gmail.com>
2015-01-30 00:33:07 +0100

    package.json and index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var port = process.argv[2];
 
 var main = function() {
   console.log('starting lan-chat');
-  node.join_network(function() {
+  node.joinNetwork(function() {
     node.publish();
   });
 };
