ebd86590d0dd7bf21ffaa5286bd9efab3e6c696d
David Dias <daviddias.p@gmail.com>
2015-01-29 23:53:43 +0100

    reader stream

--- src/bin/commands/join.js
+++ src/bin/commands/join.js
@@ -1,4 +1,6 @@
 var Command = require('ronin').Command;
+var reader = require('../../lib/reader.js');
+var duplex = require('duplex');
 
 var LanChat = Command.extend({
     desc: 'join the lan chat',
@@ -20,6 +22,12 @@ var LanChat = Command.extend({
             process.exit(1);
         }
         console.log('join command', name, auth);
+        var lineStream = duplex();
+        reader(lineStream);
+        lineStream.on('_data', function(data){
+            console.log('data', data);
+        });
+
     }
 });
 
